3049edc62df3590e3f9420884e8772883d9a0852
enzoperce35 <enzoperce35@gmail.com>
2023-07-18 11:10:26 +0800

    initialized a Javascript page load function

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 ## A 'TOP' Javascript course project which covers Javascript modules and the webpack usage
 
 ### Prospected webpage layout with basic css styling
-![alt text](project_screenshots/restaurant page basic css.png)
+![alt text](project_screenshots/restaurant_page_basic_css.png)
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Hi Webpack');
+import _ from 'lodash';
+
+function component() {
+  const content = document.getElementById('content');
+  const element = document.createElement('p');
+
+  element.innerHTML = _.join(['HTML elements and data', 'are now can be loaded', 'through Javascript'], ' ');
+
+  content.appendChild(element)
+
+  return content;
+}
+
+document.body.appendChild(component());
